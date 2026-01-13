49cf0deb0e98968ed54b7175a43f703f2c57f293
Prat78 <pratyush.singh365@gmail.com>
2026-01-13 21:40:54 +0100

    Fix formatting in games.js

--- games.js
+++ games.js
@@ -5,6 +5,6 @@ var games = {
     "basketrandom": "https://clever.pbseducation1914.org/games/basketrandom/index.html",
     "bitlife": "https://clever.pbseducation1914.org/games/bitlife/index.html",
     "basketball-stars": "https://clever.pbseducation1914.org/games/basketball-stars/index.html",
-    "bit-planes": "https://clever.pbseducation1914.org/games/bit-planes/index.html",
+    "bit-planes": "https://clever.pbseducation1914.org/games/bit-planes/index.html"
 
 };
