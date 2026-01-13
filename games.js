dc9e032852d52168e5564e180184a6e2092f5b8c
Prat78 <pratyush.singh365@gmail.com>
2026-01-13 21:41:36 +0100

    Update games.js

--- games.js
+++ games.js
@@ -1,10 +1,10 @@
 var games = {
     
-    "basketbros": "https://clever.pbseducation1914.org/games/basketbros/index.html",
+    "basketbros": "https://clever.pbseducation1914.org/games/basketbros/",
    
-    "basketrandom": "https://clever.pbseducation1914.org/games/basketrandom/index.html",
-    "bitlife": "https://clever.pbseducation1914.org/games/bitlife/index.html",
-    "basketball-stars": "https://clever.pbseducation1914.org/games/basketball-stars/index.html",
-    "bit-planes": "https://clever.pbseducation1914.org/games/bit-planes/index.html"
+    "basketrandom": "https://clever.pbseducation1914.org/games/basketrandom/",
+    "bitlife": "https://clever.pbseducation1914.org/games/bitlife/",
+    "basketball-stars": "https://clever.pbseducation1914.org/games/basketball-stars/",
+    "bit-planes": "https://clever.pbseducation1914.org/games/bit-planes/"
 
 };
