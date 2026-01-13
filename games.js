7ab992f7c05eab9e4c2e4f9d4baa34d2ccc7f151
Prat78 <pratyush.singh365@gmail.com>
2026-01-13 21:44:29 +0100

    Remove multiple games from the games object
    Removed several game entries from the games object.

--- games.js
+++ games.js
@@ -1,10 +1,7 @@
 var games = {
     
-    "basketbros": "https://clever.pbseducation1914.org/games/basketbros/",
-   
-    "basketrandom": "https://clever.pbseducation1914.org/games/basketrandom/",
-    "bitlife": "https://clever.pbseducation1914.org/games/bitlife/",
-    "basketball-stars": "https://clever.pbseducation1914.org/games/basketball-stars/",
-    "bit-planes": "https://clever.pbseducation1914.org/games/bit-planes/"
+  
+    "basketball-stars": "https://clever.pbseducation1914.org/games/basketball-stars/"
+    
 
 };
